885c99ff2c03cba8f4b3160ce444d993ba44979c
Johannes Brix <johannes.brix7@gmail.com>
2025-03-27 14:52:41 +0100

    Second Scene

--- first_manim_test.py
+++ first_manim_test.py
@@ -11,5 +11,15 @@ class Pith(Scene):
         self.play(Create(sq), run_time=3)
         self.wait()
 
+class Testing(Scene):
+    def construct(self):
+        name = Tex("Johnanes").to_edge(UL, buff=0.5)
+        sq = Square(side_length=0.5, fill_color=GREEN, fill_opacity=0.75).shift(LEFT * 3)
+        tri = Triangle().scale(0.6).to_edge(DR)
+
+        self.play(Write(name))
+        self.play(DrawBorderThenFill(sq), run_time=2)
+        self.play(Create(tri))
+        self.wait()
 
  

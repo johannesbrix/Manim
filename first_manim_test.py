af9ab1d875d463ea18fbc4fa9cc33bfab618977f
Johannes Brix <johannes.brix7@gmail.com>
2025-04-03 10:15:12 +0200

    Graphing

--- first_manim_test.py
+++ first_manim_test.py
@@ -71,3 +71,24 @@ class ValueTrackers(Scene):
         self.play(k.animate.set_value(0), run_time = 5, rate_func=smooth)
 
 
+class Graphing(Scene):
+    def construct(self):
+        plane = NumberPlane(x_range=[-4, 4, 2], x_length=7, y_range=[0, 16, 2], y_length=5).to_edge(DOWN).add_coordinates()
+
+        labels = plane.get_axis_labels(x_label="x", y_label="f(x)") 
+
+        parab = plane.plot(lambda x: x**2, x_range=[-4, 4], color=GREEN)
+
+        func_label = MathTex("f(x)=x^2").scale(0.6).next_to(parab, UR, buff=0.5).set_color(GREEN)
+
+        area = plane.get_riemann_rectangles(graph = parab, x_range=[-2, 3], dx=0.1, stroke_color=WHITE)
+
+        self.play(DrawBorderThenFill(plane))
+        self.play(Create(VGroup(parab, labels)))
+        self.wait()
+        self.play(Create(area))
+        self.wait()
+
+
+
+
